c62098633dbcfb2fc4018e32ab668b31172a8652
Shane Sveller <shanesveller@gmail.com>
2022-09-21 10:51:10 -0500

    chore(builtins): Disable file backups and swapfiles

--- pkgs/init.lua
+++ pkgs/init.lua
@@ -32,6 +32,10 @@ g.loaded_perl_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_ruby_provider = 0
 
+-- Prevent file cruft
+o.backup = false
+o.swapfile = false
+
 ---- }}}
 
 ---- Early-stage keybinds {{{
