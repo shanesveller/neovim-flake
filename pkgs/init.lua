11e985fb08aaef9262f7514773cc84851bd5ae3e
Shane Sveller <shanesveller@gmail.com>
2022-09-30 10:06:40 -0500

    feat(perf): Lazy-load gitsigns

--- pkgs/init.lua
+++ pkgs/init.lua
@@ -194,7 +194,18 @@ tel.setup({
 
 ---- Git {{{
 
-require("gitsigns").setup({})
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+	group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+	callback = function()
+		vim.fn.system("git rev-parse " .. vim.fn.expand("%:p:h"))
+		if vim.v.shell_error == 0 then
+			vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
+			vim.schedule(function()
+				require("gitsigns").setup({})
+			end)
+		end
+	end,
+})
 
 ---- }}}
 
