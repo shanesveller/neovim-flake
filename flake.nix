f30c1ae4daf509072ba41f5db19299847ebe54cf
Shane Sveller <shanesveller@gmail.com>
2022-09-30 14:45:53 -0500

    feat(workspace): Include Nix/Lua LSP binaries in devShell

--- flake.nix
+++ flake.nix
@@ -39,10 +39,16 @@
           };
           settings = {statix.ignore = [".direnv/*"];};
         };
-        devShells.default = pkgs.mkShell {
-          packages = (with pkgs; [just nix-tree]) ++ (with inputs'.pre-commit-hooks.packages; [alejandra nix-linter pre-commit statix stylua]) ++ (with config.packages; [nvfetcher]);
-          inherit (config.checks.pre-commit-check) shellHook;
-        };
+        devShells.default = let
+          lspServers = with pkgs; [rnix-lsp sumneko-lua-language-server];
+          linters = with inputs'.pre-commit-hooks.packages; [alejandra nix-linter pre-commit statix stylua];
+          local = with config.packages; [nvfetcher];
+          utilities = with pkgs; [just nix-tree];
+        in
+          pkgs.mkShell {
+            packages = utilities ++ local ++ linters ++ lspServers;
+            inherit (config.checks.pre-commit-check) shellHook;
+          };
         packages = let
           inherit (inputs'.neovim.packages) neovim;
           callVim = path: extra:
