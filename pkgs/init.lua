7e807ba3170ae5d5a916ffef89c237beb8bda459
Shane Sveller <shane@sveller.dev>
2022-12-18 15:09:47 -0600

    chore(git): Minimize netrw while preserving vim-rhubarb

--- pkgs/init.lua
+++ pkgs/init.lua
@@ -37,8 +37,8 @@ g.loaded_rrhelper = 1
 
 -- Required for vim-rhubarb :GBrowse
 -- g.loaded_netrw = 1
--- g.loaded_netrwPlugin = 1
--- g.loaded_netrwSettings = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
 
 g.loaded_node_provider = 0
 g.loaded_perl_provider = 0
