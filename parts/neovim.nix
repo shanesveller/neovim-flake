c104982954556176eb66a4a0b46c1b0c753769b7
Shane Sveller <shane@sveller.dev>
2022-12-24 13:57:23 -0600

    refactor(editor): Simplify neovimConfigured flake output to wrapping only

--- parts/neovim.nix
+++ parts/neovim.nix
@@ -12,8 +12,6 @@
       inherit neovim;
       neovimConfigured = pkgs.callPackage ../pkgs/neovim.nix {
         neovim-unwrapped = neovim;
-        inherit self;
-        inherit (config.legacyPackages) grammars;
         inherit (pkgs.nodePackages) vscode-langservers-extracted;
         tailwind-intellisense =
           pkgs.nodePackages."@tailwindcss/language-server";
--- pkgs/neovim.nix
+++ pkgs/neovim.nix
@@ -1,10 +1,10 @@
 {
   lib,
-  self,
+  makeWrapper,
   neovim-unwrapped,
-  neovimUtils,
-  vimPlugins,
-  wrapNeovimUnstable,
+  symlinkJoin,
+  # Lazy.nvim
+  gitMinimal,
   # Formatters and Linters
   alejandra,
   statix,
@@ -14,73 +14,37 @@
   sumneko-lua-language-server,
   tailwind-intellisense,
   vscode-langservers-extracted,
+  # CLI dependencies
+  fd,
+  ripgrep,
   # Build native extensions
   gcc,
   gnumake,
-  # Flake-local
-  grammars,
-}: let
-  config = neovimUtils.makeNeovimConfig {
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-
-    withNodeJs = false;
-    withPython3 = false;
-    withRuby = false;
-
-    customRC = ''
-      set rtp^=${self}/rtp
-      luafile ${self}/pkgs/init.lua
-    '';
-
-    plugins = with vimPlugins; let
-      nvim-treesitter' = nvim-treesitter.withPlugins (p:
-        (with p; [
-          tree-sitter-bash
-          tree-sitter-dot
-          tree-sitter-elisp
-          tree-sitter-erlang
-          tree-sitter-fish
-          tree-sitter-glsl
-          tree-sitter-latex
-          tree-sitter-regex
-          tree-sitter-tsx
-          tree-sitter-typescript
-        ])
-        ++ (with grammars; [
-          tree-sitter-eex
-          tree-sitter-just
-          tree-sitter-norg
-          tree-sitter-norg-meta
-          tree-sitter-norg-table
-        ]));
-    in [
-      # Syntax highlighting
-      nvim-treesitter'
-    ];
-  };
-in
-  wrapNeovimUnstable neovim-unwrapped (config
-    // {
-      wrapperArgs =
-        config.wrapperArgs
-        ++ [
-          "--suffix"
-          "PATH"
-          ":"
-          (lib.makeBinPath [
-            # LSP and linters
-            alejandra
-            nil
-            statix
-            stylua
-            sumneko-lua-language-server
-            tailwind-intellisense
-            vscode-langservers-extracted
-            # Compiling native extensions
-            gcc
-            gnumake
-          ])
-        ];
-    })
+}:
+symlinkJoin {
+  name = "nvim";
+  paths = [neovim-unwrapped];
+  buildInputs = [makeWrapper];
+  postBuild = ''
+    wrapProgram $out/bin/nvim \
+      --suffix PATH ":" ${
+      lib.makeBinPath [
+        # CLI dependencies
+        fd
+        ripgrep
+        # LSP and linters
+        alejandra
+        gitMinimal
+        nil
+        statix
+        stylua
+        sumneko-lua-language-server
+        tailwind-intellisense
+        vscode-langservers-extracted
+        # Compiling native extensions
+        gcc
+        gnumake
+      ]
+    }
+  '';
+}
